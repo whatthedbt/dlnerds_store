0eb045c854bb3c736525d4ff2b377143aad3ef76
Muhammad Khan <joesparksql801@gmail.com>
2026-05-18 00:13:28 +0000

    Jinja for loop applied to dim_product

--- models/gold/dim_product.sql
+++ models/gold/dim_product.sql
@@ -1,4 +1,4 @@
-{% set tier=[
+{% set tiers=[
     {'name': 'low', 'min': 0, 'max': 29.99},
     {'name': 'mediaum', 'min': 30, 'max': 59.99},
     {'name': 'high', 'min': 60, 'max': 199.99}
@@ -10,10 +10,13 @@ with cte_read_product as(
 
 cte_transform_product as(
     select id, name, code, category, price, currency, 
-    case
-    when price between {{ tier[0].min}} and {{ tier[0].max}} then '{{ tier[0].name }}'
-    when price between {{ tier[1].min}} and {{ tier[1].max }} then '{{ tier[1].name }}'
-    when price between {{ tier[2].min }} and {{ tier[2].max}} then '{{ tier[2].name }}'
+    case -- This complexcity can be avoided by using Jinja for loop
+    -- when price between {{ tiers[0].min}} and {{ tiers[0].max}} then '{{ tiers[0].name }}'
+    -- when price between {{ tiers[1].min}} and {{ tiers[1].max }} then '{{ tiers[1].name }}'
+    -- when price between {{ tiers[2].min }} and {{ tiers[2].max}} then '{{ tiers[2].name }}'
+    {% for tier in tiers %}
+        when price between {{ tier.min }} and {{ tier.max }} then '{{ tier.name }}'
+    {% endfor %}
     else null
     end as tier, 
     color, created_at, updated_at from cte_read_product
